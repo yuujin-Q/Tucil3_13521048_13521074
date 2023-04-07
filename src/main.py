8b84931a03cfa82a1cc5c5bfd7870563ef22ce18
yuujin-Q <13521074@mahasiswa.itb.ac.id>
2023-04-07 23:48:45 +0700

    feat: ucs search

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from graph import *
 from graph_reader import *
+from ucs import *
 
 reader = GraphReader()
 reader.read_graph_file()
@@ -7,4 +8,10 @@ reader.print_reader_info()
 
 my_graph = reader.get_location_graph()
 
-my_graph.display_graph(with_weights=False)
+# my_graph.display_graph(with_weights=False)
+
+print()
+ucs_graph = UCS(my_graph, True)
+ucs_graph.ucs_search("braga","celengan")
+print()
+ucs_graph.print_solution()
